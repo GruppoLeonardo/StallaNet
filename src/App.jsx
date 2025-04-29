d51657b54b25800fc77d4adaab253914041c19ba
GruppoLeonardo <grupposviluppoleonardo@gmail.com>
2025-04-29 16:49:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import Accesso from './pages/Accessi&Timbri/Accesso.jsx';
 import TimbraEntrata from './pages/Accessi&Timbri/TimbraEntrata.jsx';
 import TimbraUscita from './pages/Accessi&Timbri/TimbraUscita.jsx';
 import HomeOperaio from './pages/Operaio/HomeOperaio.jsx';
+import GestorePagHome from './pages/Gestore/GestorePagHome.jsx';
+import GestorePagMucche from './pages/Gestore/GestorePagMucche.jsx';
+import HomeDirettore from './pages/Direttore/HomeDirettore.jsx';
+
 
 //COMPONENTE APP 
 function App() {
@@ -18,6 +22,9 @@ function App() {
         <Route path="/TimbraEntrata" element={<TimbraEntrata />} />
         <Route path="/TimbraUscita" element={<TimbraUscita />} />
         <Route path="/HomeOperaio" element={<HomeOperaio />} />
+        <Route path="/GestorePagHome" element={<GestorePagHome />} />
+        <Route path="/GestorePagMucche" element={<GestorePagMucche />} />
+        <Route path="/HomeDirettore" element={<HomeDirettore />} />
       </Routes>
     </Router>
   );
