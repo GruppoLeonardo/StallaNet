97dfecb0036f34ad997ac6f941f22befdf32d8da
GruppoLeonardo <grupposviluppoleonardo@gmail.com>
2025-06-08 19:09:57 +0200

    Aggiunta di file mancanti nel src

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,10 @@ import TimbraEntrata from './pages/Accessi&Timbri/TimbraEntrata.jsx';
 import TimbraUscita from './pages/Accessi&Timbri/TimbraUscita.jsx';
 import HomeOperaio from './pages/Operaio/HomeOperaio.jsx';
 import GestorePagHome from './pages/Gestore/GestorePagHome.jsx';
+import GestorePagOperai from './pages/Gestore/GestorePagOperai.jsx';
 import GestorePagMucche from './pages/Gestore/GestorePagMucche.jsx';
 import HomeDirettore from './pages/Direttore/HomeDirettore.jsx';
+
 
 
 //COMPONENTE APP 
@@ -23,6 +25,7 @@ function App() {
         <Route path="/TimbraUscita" element={<TimbraUscita />} />
         <Route path="/HomeOperaio" element={<HomeOperaio />} />
         <Route path="/GestorePagHome" element={<GestorePagHome />} />
+        <Route path="/GestorePagOperai" element={<GestorePagOperai />} />
         <Route path="/GestorePagMucche" element={<GestorePagMucche />} />
         <Route path="/HomeDirettore" element={<HomeDirettore />} />
       </Routes>
